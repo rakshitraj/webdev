83f0e7872b11019c2d430d3aeadfb2ede8afaf7d
rakshitraj <rraj.rakshit@gmail.com>
2020-05-18 11:02:29 +0530

    added formal parmater for title to header.html

--- web.py
+++ web.py
@@ -1,18 +1,24 @@
 # WebApp
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 # Render Homepage
 @app.route("/")
 def index():
-    return render_template("index.html")
+    title = 'Homepage'
+    return render_template("index.html", title=title)
+
 
 # Render About page
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    title = 'About'
+    return render_template('about.html', title=title)
+
 
 # Render contacts page
 @app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    title = 'Contact'
+    return render_template('contact.html', title=title)
